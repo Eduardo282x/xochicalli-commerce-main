9e055aba71fb6a7be036fd0fc0fb5ff3bfb8949a
Eduardo <eduardokunmixyeah@gmail.com>
2024-04-14 23:03:16 -0400

    change config firebase

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -6,13 +6,13 @@ import { FirebaseStorage, getStorage, ref, uploadBytes, getDownloadURL } from 'f
 import { v4 } from "uuid";
 
 export const firebaseConfig = {
-  apiKey: import.meta?.env?.VITE_FIREBASE_APIKEY ?? "AIzaSyBQxvYvalPyyVzzYu6ey9XrHllO1XHJU5I",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN ?? "xochicalli-commerce.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECTID ?? "xochicalli-commerce",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET ?? "xochicalli-commerce.appspot.com",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID ?? "1065447557124",
-  appId: import.meta.env.VITE_FIREBASE_APPID ?? "1:1065447557124:web:062473512037026f214d51",
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID ?? "G-0NCXM28373",
+  apiKey: "AIzaSyBQxvYvalPyyVzzYu6ey9XrHllO1XHJU5I",
+  authDomain: "xochicalli-commerce.firebaseapp.com",
+  projectId: "xochicalli-commerce",
+  storageBucket: "xochicalli-commerce.appspot.com",
+  messagingSenderId: "1065447557124",
+  appId: "1:1065447557124:web:89a3b272c996a79f214d51",
+  measurementId: "G-L3DNKLN90J",
 };
 
 export const app: FirebaseApp = initializeApp(firebaseConfig);
